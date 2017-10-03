62410aa3a0924bdcb2712197e9b6c7a1c3a72181
f-leborgne <32433180+f-leborgne@users.noreply.github.com>
2017-10-03 15:01:01 +0200

    Add files via upload

---
+++ server.r
@@ -0,0 +1,141 @@
+# check if necessary packages are installed and install if not
+list.of.packages <- c("shiny", "shinyjs", "JM")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# load packages
+lapply(list.of.packages, require, character.only = T)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+	
+	load("JM_EJEquesurv.RData")	
+	
+	rv <- reactiveValues()  
+	
+	observe({
+		if( !isTruthy(input$Dgreffeb) | input$rangGreffe=="" | is.na(input$AgeR_calc) | is.null(input$sexeD) | is.null(input$cardioVasc) | is.null(input$antiClassI) | is.null(input$rejet1an) | is.na(input$creat_3m) | !isTruthy(input$Dsuivi_12m) | is.na(input$creat_12m) ){
+			rv$validform <- FALSE
+		}else{
+			temp <- TRUE
+			if(input$ajouter != 0){
+				for(i in 1:input$ajouter){
+					if(!isTruthy(input[[paste0("Dsuivi_", i)]])){temp <- FALSE}
+					if(!isTruthy(input[[paste0("creat_", i)]])){temp <- FALSE}
+				}				
+			}
+			rv$validform <- temp
+		}
+	})
+	
+	output$control <- renderText({
+		if( rv$validform == FALSE ){
+			"<p style='font-size:18px;'>Veuillez compléter tous les champs</p>"
+		}
+	})
+	
+	observe({
+		if(rv$validform){
+			Dsuivi <- input$Dsuivi_12m
+			for(i in 1:input$ajouter){
+				Dsuivi <- c(Dsuivi, input[[paste0("Dsuivi_", i)]])
+			}
+			rv$Dsuivi <- Dsuivi
+			
+			creat <- input$creat_12m
+			for(i in 1:input$ajouter){
+				creat <- c(creat, input[[paste0("creat_", i)]])
+			}
+			rv$creat <- creat
+		}
+	})
+	
+	observe({
+		if(rv$validform & input$ajouter != 0){
+			updateSliderInput(session, "slider_graph", value = (input$ajouter+1), min = 1, max = (input$ajouter+1), step = 1)
+			show("slider_graph")
+		}else{
+			hide("slider_graph")
+		}
+	})
+	
+	bdd.long <- reactive({
+		if(rv$validform){
+			data.frame(
+				clef = "111", 
+				Dsuivi = rv$Dsuivi,
+				creat = rv$creat,
+				Dgreffeb = input$Dgreffeb, 
+				yearGreffe = format(input$Dgreffeb,"%Y"),
+				periode2008 = ifelse(as.numeric(format(input$Dgreffeb,"%Y"))<2008,1,0),
+				rangGreffe = as.numeric(input$rangGreffe),
+				AgeR_calc = input$AgeR_calc,
+				cardioVasc = as.numeric(input$cardioVasc),
+				sexeD = as.numeric(input$sexeD),
+				antiClassI = as.numeric(input$antiClassI),
+				Rejet1an = as.numeric(input$rejet1an),
+				creat_3m = input$creat_3m,
+				Dsuivi_12mb = input$Dsuivi_12m,
+				Evt=0,
+				TpsEvtAns_depM12 = as.numeric(max(as.numeric(rv$Dsuivi - input$Dsuivi_12m)/365.25)),
+				tps_postM12 = as.numeric((rv$Dsuivi - input$Dsuivi_12m)/365.25),
+				AgeR_S = input$AgeR_calc/13.6,		
+				creat_3m_S = input$creat_3m/53.4	
+			)
+		}
+	})
+	
+	#output$base <- renderTable(bdd.long())
+	
+	#output$base <- DT::renderDataTable(bdd.long(), options=list(pageLength=10, scrollX = TRUE))
+	
+	
+	observeEvent(input$ajouter, {		
+		insertUI(
+			selector = '#toto',
+			where = "beforeBegin",
+			ui = fluidPage(
+				column(3,div(style="display:inline-grid;margin-top:10px;font-size:14px;font-weight: 700;", paste0("Visite ", input$ajouter+1))),
+				column(9,
+				div(style="display:inline-block;margin-left:20px;margin-top:-15px;",dateInput(inputId=paste0("Dsuivi_", input$ajouter), label="",  format = "dd/mm/yyyy", value="", width=130)),
+				div(style="display:inline-block;margin-left:20px;margin-top:-15px;",numericInput(inputId=paste0("creat_", input$ajouter), label="", value="", min=20, max=2000, width=130))
+				)
+			)
+		);
+	})
+	
+	
+	horizon<-5			
+	
+	output$graph <- renderPlot({
+		if(rv$validform){
+			if(dim(bdd.long())[1]>=1){
+				ND <- bdd.long()
+				survPreds <- vector("list", nrow(ND))
+				
+				for (i in 0:(min(nrow(ND), 5)-1)){
+					set.seed(123)
+					survPreds[[i+1]]<-survfitJM(JM_EJEquesurv, newdata=ND[1:(i+1),], last.time=round(as.numeric(ND[(i+1),"tps_postM12"]),2),idVar="clef",survTimes= round(as.numeric(ND[(i+1),"tps_postM12"]),2)+horizon,simulate=T)
+				}
+				
+				if(input$ajouter != 0){
+					ind <- input$slider_graph
+				}else{
+					ind <- 1
+				}
+				
+				plot.survfitJM(survPreds[[ind]], estimator="median", conf.int=TRUE, include.y=TRUE, ylab="", ylab2="", xlab="", main=paste("Visite ", ind))
+				
+				mtext("log(serum of creatinine)", side=2, line=-1, outer=TRUE)
+				mtext("Survival Probability", side=4, line=-1, outer=TRUE)				
+			}
+		}
+	},width = function() {
+		min(session$clientData$output_survie_width,500)
+	})	
+
+	
+})
+
---
+++ ui.R
@@ -0,0 +1,85 @@
+
+dateInputRow<-function (inputId, label, format) 
+{
+    div(style="display:inline-block",id = inputId, 
+        tags$label(label, `for` = inputId), 
+        tags$input(type = "date", format = format, class="form-inline"))
+}
+
+numericInputRow<-function (inputId, label, value, min, max) 
+{
+    div(style="display:inline-block",
+        tags$label(label, `for` = inputId), 
+        tags$input(id = inputId, type = "numeric", value = value, min = min, max = max, class="input-small", width=20))
+}
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(	
+	useShinyjs(),
+	
+	fluidPage( # un fuildpage en plus pour centrer le titre
+		titlePanel("", "Prédiction dynamiques - DIVAT")
+	),
+	
+	# Sidebar with a slider input for the number of bins
+	sidebarLayout(
+	
+		div( id="sidebar",sidebarPanel(
+			width=3,
+			dateInput("Dgreffeb", "Date de greffe", format = "dd/mm/yyyy"),
+			tags$label(br(),""),
+			selectInput("rangGreffe", "Transplantation rank", c("Choose"="",1,2,3,4,5)),
+			tags$label(br(),""),
+			numericInput("AgeR_calc", "Recipient age at transplantation (year)", "", min=1, max=90),
+			tags$label(br(),""),
+			radioButtons("sexeD", "Sexe du donneur", choices = c("Femme"=0, "Homme"=1), selected = character(0), inline = TRUE),
+			tags$label(br(),""),
+			radioButtons("cardioVasc", "Antécédents cardio-vasculaires", choices = c("Non"=0, "Oui"=1), selected = character(0), inline = TRUE),
+			tags$label(br(),""),
+			radioButtons("antiClassI", "Immunisation anti-class I", choices = c("Non"=0, "Oui"=1), selected = NULL, inline = TRUE),
+			tags$label(br(),""),
+			radioButtons("rejet1an", "Rejet dans la première année", choices = c("Non"=0, "Oui"=1), selected = NULL, inline = TRUE),
+			tags$label(br(),""),
+			numericInput("creat_3m", "Créatinine à 3 mois (µmol/l)", "", min=20, max=2000)			
+		)),  
+		mainPanel(
+			width=9,
+			wellPanel(
+				fluidPage(
+					tags$head(tags$style(HTML('.form-group {margin:0px;}'))),
+					tags$head(tags$style(HTML('.selectize-control {margin-bottom:-5px;}'))),
+					column(3,
+						div(style="display:inline-grid;", tags$label(br(),HTML("<p style='height:25px;padding-top:10px;'>Visite des 12 mois post-greffe</p>")))
+					),
+					column(9,
+						div(style="display:inline-block;margin-left:20px;",dateInput(inputId="Dsuivi_12m", label="Date", format = "dd/mm/yyyy", width=130)),
+						div(style="display:inline-block;margin-left:20px;",numericInput(inputId="creat_12m", label="Créatinine (µmol/l)", value="", min=20, max=2000, width=130))
+					)
+				),
+				div(id="toto", br()),
+				actionButton("ajouter", "Ajouter un suivi")
+			),
+			htmlOutput("control"),
+			#DT::dataTableOutput("base"),
+			plotOutput("graph",height=500),
+			hidden(sliderInput("slider_graph","Visite",min=1,max=4, value=4, ticks=FALSE)),
+			br(br())
+		)
+	)
+))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
